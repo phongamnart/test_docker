63c48cd19b8ac8ca9c535b46eeb59d8aadf1c243
phongamnart <pknart17@gmail.com>
2023-10-06 02:19:12 +0700

    edit

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Product(BaseModel):
+    barcode: str
+    name_product: str
+    price: float
+
+products = [
+    Product(barcode="12345", name_product="Singha", price=15.00),
+    Product(barcode="67890", name_product="Noodle", price=7.00),
+]
+
+@app.get("/")
+def index():
+    return products
+
+# สร้างเส้นทาง HTTP POST เพื่อเพิ่มข้อมูลสินค้า
+@app.post("/add-product/")
+def add_product(product: Product):
+    products.append(product)
+    return {"message": f"Product {product.name_product} has been added."}
+
+# เส้นทาง HTTP PUT เพื่อแก้ไขข้อมูลสินค้า
+@app.put("/update-product/{product_name}")
+def update_product(product_name: str, updated_product: Product):
+    for i, product in enumerate(products):
+        if product.name_product == product_name:
+            products[i] = updated_product
+            return {"message": f"Product {product_name} has been updated."}
+    return {"error": f"Product {product_name} not found."}
+
+# เส้นทาง HTTP DELETE เพื่อลบข้อมูลสินค้า
+@app.delete("/delete-product/{product_name}")
+def delete_product(product_name: str):
+    for product in products:
+        if product.name_product == product_name:
+            products.remove(product)
+            return {"message": f"Product {product_name} has been deleted."}
+    return {"error": f"Product {product_name} not found."}
+
+# เส้นทางสำหรับดูรายการสินค้าทั้งหมด
+@app.get("/products/")
+def get_products():
+    return {"products": [product.dict() for product in products]}
